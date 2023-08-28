f4da12f7548b319358e1b39aee8ff5f36f8ad4fb
harshil-sinha <130955042+harshil-sinha@users.noreply.github.com>
2023-08-28 09:44:20 +0530

    Update App.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Route,Routes } from "react-router-dom";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import Navbar2 from "./Navbar2";
+import Home from "./Home";
+const App = () =>{
+    return (
+        <>
+            <Navbar2/>
+            <Routes>
+                <Route path='/*' element={<Home/>}/>
+                <Route path="/signup" element={<SignUp/>}/>
+                <Route path="/login" element={<Login/>}/>
+            </Routes>
+        </>
+    )
+}
+export default App;
