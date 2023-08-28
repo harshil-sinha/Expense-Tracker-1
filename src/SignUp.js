c4d1197a22409cd031af8f3294e59c9000974964
harshil-sinha <130955042+harshil-sinha@users.noreply.github.com>
2023-08-28 17:14:10 +0530

    Update SignUp.js

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,31 +1,56 @@
 import React from 'react'
-import "./index.css";
+import "./SignUp.css";
+import {database} from './firebase.js';
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import {
     MDBContainer,
     MDBCol,
     MDBRow,
     MDBBtn,
-    MDBIcon,
     MDBInput,
   } from 'mdb-react-ui-kit';
+import { useNavigate } from 'react-router-dom';
 
 const SignUp = () =>{
+    const history = useNavigate()
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        const email = event.target.email.value;
+        const pass = event.target.pass.value;
+        const Cpass = event.target.Cpass.value;
+
+        if(pass!==Cpass){
+            alert("Confirm Password Does Not Match")
+        }
+
+        createUserWithEmailAndPassword(database,email,pass,Cpass).then(()=>{
+            // console.log(data,"authData");
+            alert("Sign Up Successfully")
+            history('/login');
+        }).catch((err)=>{
+            alert(err.code);
+        })
+        
+    }
     return (
         <>
          <MDBContainer fluid className="p-3 my-5">
             <MDBRow>
-            <MDBCol col='10' md='6'>
+            <MDBCol md='6'>
                 <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg" class="img-fluid" alt="Phone image" />
             </MDBCol>
+            <MDBCol md='6' className='text-center'>
+            <form onSubmit={(event) =>{
+                handleSubmit(event)
+            }}>
+            <h1 className='mb-3'>Sign Up</h1>
+            <MDBInput wrapperClass='mb-4' name='email' id='formControlLg' type='email' size="lg" placeholder='Email Id'/>
+            <MDBInput wrapperClass='mb-4' name="pass" id='formControlLg' type='password' size="lg" placeholder='Password'/>
+            <MDBInput wrapperClass='mb-4' name="Cpass" id='formControlLg' type='password' size="lg" placeholder='Confirm Password'/>
 
-            <MDBCol col='4' md='6'>
-            <h1 className='mb-3 text-center'>Sign Up</h1>
-            <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg"/>
-            <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg"/>
-            <div className="d-flex justify-content-between mx-4 mb-4">
-            </div>
-
-            <MDBBtn className="mb-4 w-100" size="lg">Sign Up</MDBBtn>
+            <MDBBtn className="mb-4" size="lg" >Sign Up</MDBBtn>
+            </form>
         </MDBCol>
         </MDBRow>
         </MDBContainer>
