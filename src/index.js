48e9ebe2c6809bec6082fb55554b6541813d4c48
harshil-sinha <130955042+harshil-sinha@users.noreply.github.com>
2023-08-28 09:45:47 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar2 from './Navbar2';
-import SignUp from './SignUp';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navbar2 />
-    <SignUp/>
+   <BrowserRouter>
+    <App/>
+   </BrowserRouter>
   </React.StrictMode>
 );
