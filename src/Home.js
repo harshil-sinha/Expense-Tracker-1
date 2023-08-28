8be372150b4841e87719f2d15cfe3576786f2934
harshil-sinha <130955042+harshil-sinha@users.noreply.github.com>
2023-08-28 17:12:52 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,28 @@
-import { MDBContainer, MDBCol, MDBRow} from 'mdb-react-ui-kit';
+import { MDBContainer, MDBCol, MDBRow,MDBBtn} from 'mdb-react-ui-kit';
 import React from 'react';
+import { signOut } from 'firebase/auth';
+import { database } from './firebase';
+import { useNavigate } from 'react-router-dom';
 const Home = () =>{
+    const history = useNavigate();
+
+    const handleClick = () =>{
+        signOut(database).then(()=>{
+            alert("Logout Successfully")
+            history("/login")
+        }).catch((err)=>{
+            alert(err.code);
+        })
+    }
     return (
         <>
+         <MDBContainer className="my-2 text-end">
+         <MDBRow>
+         <MDBCol>
+         <MDBBtn className="mb-4" size="md" onClick={handleClick}>LogOut</MDBBtn>
+         </MDBCol>
+         </MDBRow>
+         </MDBContainer>
         <MDBContainer className="my-5 text-center">
             <h1 className='mb-3'>Welcome</h1>
             <MDBRow>
