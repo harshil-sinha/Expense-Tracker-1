b3dc3833204b25b9b32f175f88f4d20d5247086c
harshil-sinha <130955042+harshil-sinha@users.noreply.github.com>
2023-08-29 10:23:06 +0530

    Update Form.js

--- src/Form.js
+++ src/Form.js
@@ -1,30 +1,46 @@
-import React from 'react';
-import {
-  MDBInput,
-  MDBCol,
-  MDBRow,
-  MDBBtn,
-  MDBContainer,
-} from 'mdb-react-ui-kit';
+import React,{useState} from 'react';
+import { Box, Typography, styled } from '@mui/material';
+import Balance from './compents/Balance';
+import ExpenseCard from './compents/ExpenseCard';
+import NewTransactions from './compents/NewTransaction';
+import Transaction from './compents/Transaction';
 
+const Header = styled(Typography)`
+    font-size : 30px;
+    color : blue;
+    text-transform : uppercase;
+    text-align : center;
+
+`
+
+const Component = styled(Box)`
+
+    display:flex;
+    background:#e5e4e0;
+    padding:10px;
+    margin:auto;
+    &>div{
+      height:70vh;
+      width:50%;
+      padding:10px;
+    }
+
+`
 export default function Form() {
+  const [transaction, setTransaction] = useState([]);
   return (
-    <MDBContainer fluid className='mt-5'>
-    <form>
-    <MDBRow className='mb-4 justify-content-end'>
-        <MDBCol className='col-md-4'>
-        <label htmlFor="">Enter Full Name:</label>
-          <MDBInput id='form6Example1' />
-        </MDBCol>
-        <MDBCol className='col-md-4'>
-        <label htmlFor="">Profile Photo URL</label>
-          <MDBInput id='form6Example2'/>
-          <MDBBtn type='submit' block className='mt-3'>Sign in</MDBBtn>
-        </MDBCol>
-      </MDBRow>
-    {/* </MDBCol>
-    </MDBRow> */}
-    </form>
-    </MDBContainer>
+   <>
+      <Header>Welcome To Expense Tracker</Header>
+      <Component>
+        <Box>
+          <Balance transaction={transaction}/>
+          <ExpenseCard transaction={transaction}/>
+          <NewTransactions setTransaction={setTransaction}/>
+        </Box>
+        <Box>
+           <Transaction transaction={transaction} setTransaction={setTransaction}/>
+        </Box>
+      </Component>
+   </>
   );
 }
