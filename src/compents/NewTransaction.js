ba72df4b7d02c9f3e036d0d490380f8267977004
harshil-sinha <130955042+harshil-sinha@users.noreply.github.com>
2023-08-29 12:56:14 +0530

    Update NewTransaction.js

--- src/compents/NewTransaction.js
+++ src/compents/NewTransaction.js
@@ -1,4 +1,4 @@
-import { Box, Button, TextField, Typography,styled} from "@mui/material";
+import { Box, Button, TextField, Typography,styled,FormControl, InputLabel,Select, MenuItem} from "@mui/material";
 import { useState } from "react";
 
 const Container = styled(Box)`
@@ -14,10 +14,12 @@ const Container = styled(Box)`
 const NewTransactions = ({setTransaction}) =>{
     const [text, setText] = useState('');
     const [amount,setAmount] = useState();
+    const [type,setType] = useState();
 
     const addTransaction = () =>{
         const transaction = {
             id: Math.floor(Math.random()*1000000),
+            type:type,
             text:text,
             amount:+amount
         }
@@ -26,9 +28,20 @@ const NewTransactions = ({setTransaction}) =>{
     }
 
     return (
-        <>
+        <>  
             <Container className="text-center">
                 <Typography variant="h5" className="text-center mt-3 mb-2">New Transaction</Typography>
+                <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Type</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    label="Age"
+                    onChange={(e) => setType(e.target.value)}>
+                    <MenuItem value="Expense" >Expense</MenuItem>
+                    <MenuItem value="Income">Income</MenuItem>
+                </Select>
+                </FormControl>
                 <TextField label="Enter Expense" required onChange={(e) => setText(e.target.value)}/>
                 <TextField label="Enter Amount" required onChange={(e) => setAmount(e.target.value)}/>
                 <Button variant="contained" onClick={addTransaction}>Add Transaction</Button>
